63a9c3c930381b18c5f9982711c00aa792c90a70
vjotov <vjotov@1393505e-1ba9-11df-8ce3-277986988311>
2010-04-09 20:24:42 +0000

    refactoring - result 6

--- trunk/gui/DesktopApplication1/src/com/jotov/versia/gui2/command/ViewVersionedObjectHistory.java
+++ trunk/gui/DesktopApplication1/src/com/jotov/versia/gui2/command/ViewVersionedObjectHistory.java
@@ -6,15 +6,40 @@ package com.jotov.versia.gui2.command;
  * Purpose: Defines the Class ViewVersionedObjectHistory
  ***********************************************************************/
 
+import com.jotov.versia.WorkEnvironment;
+import com.jotov.versia.json.JSONConnection;
 import java.util.*;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class ViewVersionedObjectHistory implements ICommand {
 
-    public Object doRequest() {
-        throw new UnsupportedOperationException("Not supported yet.");
+    public Object doRequest() throws JSONException {
+        WorkEnvironment we = WorkEnvironment.getWorkEnvironment();
+        int uid = we.getUid();
+
+        JSONConnection jc = new JSONConnection();
+        Map params = new HashMap();
+        params.put("vo_id", we.getCurrentVersionedObjectID());
+        params.put("release_id", we.getCurrentRelease());
+        jc.prepareJSONRequest("viewVersionedObjectHistory", params, uid);
+        JSONObject jResponce = jc.doRequest(null);
+        JSONObject err = jResponce.getJSONObject("error");
+        
+        //@todo - to check what to do with the result
+        JSONArray HistoryItems = jResponce.getJSONArray("result");
+
+        int code = err.getInt("code");
+        if (code == 0) {
+            return new Integer(1);
+        } else {
+            System.err.println("JSON ERROR loadReleases - code:" + code + "; message:" + err.get("message").toString());
+            return null;
+        }
     }
 
     public void setParameters(HashMap params) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        return;
     }
 }

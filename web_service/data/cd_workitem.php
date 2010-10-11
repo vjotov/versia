69341e43716401edb5af203cc58b8fad1446304f
vjotov@gmail.com <vjotov@gmail.com@1393505e-1ba9-11df-8ce3-277986988311>
2010-10-11 20:43:07 +0000

    create workitem

--- web_service/data/cd_workitem.php
+++ web_service/data/cd_workitem.php
@@ -1,11 +1,26 @@
 <?php
 
 class cd_workitem {
-	static public function create_workitem($release_id, $vo_id, $vp_vp, $wi_name, $ws_id) {
+	static public function create_workitem($release_id, $vo_id, $wi_name) {
 		global $mdb; 
+		//some checks
+		$query_0 = "SELECT count(*) AS cc FROM t_workitem WHERE initiator_vo_id='".$vo_id."' AND release_id='".$release_id."' "; 
+		$resultset_0 = $mdb->query($query_0);
+		if(PEAR::isError($resultset_0)) {
+			$err_['code'] = 2;
+			$err_['message'] = 'Failed to issue query, error message : ' . $resultset_0->getMessage();
+			$err_['message'] .= '\n cd_workitem::create_workitem - 2';
+			return $err_;
+		}
+		$wi_count = $resultset_0->fetchAll(MDB2_FETCHMODE_ASSOC);
+		if($wi_count[0]['cc'] != 0) { 
+      $err_['code'] = 3;
+      $err_['message']  = 'This object is already a workitem for the selected release';
+      return $err_;
+		}
 		
-		$query = "INSERT INTO t_workitem (initiator_vo_id, initiator_vp_id, wi_name, release_id) "
-						." VALUES ('".$vo_id."', '".$vp_vp."', '".$wi_name."', '".$release_id."')";
+		$query = "INSERT INTO t_workitem (initiator_vo_id, wi_name, release_id) "
+						." VALUES ('".$vo_id."', '".$wi_name."', '".$release_id."')";
 		$resultset = $mdb->exec($query); 
 		if(PEAR::isError($resultset)) {
 			$err_['code'] = 1;
@@ -15,8 +30,22 @@ class cd_workitem {
 		}
 		return array('code' => 0, 'message' => 'Sucessful');
 	}
-	static public function update_workitem($wi_id, $wi_name) {
+	static public function update_workitem($wi_id, $wi_name, $release_id) {
 		global $mdb;
+		$query_0 = "SELECT count(*) AS cc FROM t_workitem WHERE initiator_vo_id='".$vo_id."' AND release_id='".$release_id."' "; 
+		$resultset_0 = $mdb->query($query_0);
+		if(PEAR::isError($resultset_0)) {
+			$err_['code'] = 2;
+			$err_['message'] = 'Failed to issue query, error message : ' . $resultset_0->getMessage();
+			$err_['message'] .= '\n cd_workitem::create_workitem - 2';
+			return $err_;
+		}
+		$wi_count = $resultset_0->fetchAll(MDB2_FETCHMODE_ASSOC);
+		if($wi_count[0]['cc'] != 1) { 
+      $err_['code'] = 3;
+      $err_['message']  = 'This object is not a workitem for the selected release';
+      return $err_;
+		}
 		
 		$query = "UPDATE t_workitem SET wi_name='".$wi_name."' WHERE wi_id='".$wi_id."' ";
 		$resultset = $mdb->exec($query); 
@@ -55,6 +84,7 @@ class cd_workitem {
 		}
 		return array('code' => 0, 'message' => 'Sucessful');
 	}
+	/*
 	static public function get_workitem_list($release_id) {
 		global $mdb; 
 			
@@ -70,6 +100,7 @@ class cd_workitem {
 		return array ('error' => array('code' => 0, 'message' => 'Sucessful'),
 			'workitem_list' => $wi_list);
 	}
+	//*/
 	static public function get_attached_workitem_list($ws_id) {
 		global $mdb;
 		$query = "SELECT wi_id, vo_id, wi_name AS name "
@@ -86,11 +117,16 @@ class cd_workitem {
 		return array('error' => array('code' => 0, 'message' => 'Successful'),
 			'workitem_list' => $wi_list );
 	}
-	static public function get_not_attached_workitem_list($ws_id) {
+	static public function get_not_attached_workitem_list($ws_id, $release_id) {
 		global $mdb;
 			
-		$query = "SELECT wi_id, vo_id, vp.name AS name, vp.constructs AS constructs "
-				." FROM v_workitem_distribution INNER JOIN t_versioned_primitive vp USING(vo_id) WHERE ws_id='".$ws_id."' AND is_active='0'  ORDER BY constructs ASC, name DESC";	
+		$query = "SELECT wi_id, vo_id, "
+        ." (SELECT constructs FROM t_versioned_primitive WHERE vo_id=wid.vo_id AND vp_id=wid.vp_id) AS constructs, "
+        ." (SELECT name FROM t_versioned_primitive WHERE vo_id=wid.vo_id AND vp_id=wid.vp_id) AS name"
+				." FROM v_workitem_distribution wid "
+				." WHERE ws_id='".$ws_id."' AND release_id='".$release_id."' AND is_active='0'  "
+				." ORDER BY constructs ASC, name DESC";	
+
 		$resultset = $mdb->query($query); 
 		if(PEAR::isError($resultset)) {
 			$err_['code'] = 1;

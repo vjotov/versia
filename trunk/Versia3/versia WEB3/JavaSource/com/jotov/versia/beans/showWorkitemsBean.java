6bc6d10a241e1b076d09001fcc39672e96e9067d
vjotov@gmail.com <vjotov@gmail.com@1393505e-1ba9-11df-8ce3-277986988311>
2011-11-27 15:06:38 +0000

    start of history

---
+++ trunk/Versia3/versia WEB3/JavaSource/com/jotov/versia/beans/showWorkitemsBean.java
@@ -0,0 +1,12 @@
+package com.jotov.versia.beans;
+
+public class showWorkitemsBean {
+	public String showWorkitems() {
+		return "show_woritems";
+	}
+
+	public String showWorkspace() {
+		return "show_workspace";
+	}
+
+}

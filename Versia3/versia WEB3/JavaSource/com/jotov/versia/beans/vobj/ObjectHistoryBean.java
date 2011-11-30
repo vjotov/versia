8b6634f886493566cf47cd88cc0c3882a12a77f8
vjotov@gmail.com <vjotov@gmail.com@1393505e-1ba9-11df-8ce3-277986988311>
2011-11-30 14:08:30 +0000

    show version history Part 2 - cause of versions

--- Versia3/versia WEB3/JavaSource/com/jotov/versia/beans/vobj/ObjectHistoryBean.java
+++ Versia3/versia WEB3/JavaSource/com/jotov/versia/beans/vobj/ObjectHistoryBean.java
@@ -1,11 +1,13 @@
 package com.jotov.versia.beans.vobj;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 import javax.persistence.Query;
 
 import org.richfaces.component.UIScrollableDataTable;
+import org.richfaces.model.selection.SimpleSelection;
 
 import com.jotov.versia.beans.UserSessionBean;
 import com.jotov.versia.beans.aDBbean;
@@ -16,14 +18,32 @@ import com.jotov.versia.orm.VersionArc;
 public class ObjectHistoryBean extends aDBbean {
 	private UserSessionBean session;
 	private List<HistoryItem> historyItems = new ArrayList<HistoryItem>();
+	private ArrayList<HistoryItem> selectedItems;
 	private UIScrollableDataTable table;
-	private HistoryItem selection = null;
+	private SimpleSelection selection = new SimpleSelection();
+	private HistoryItem selectedItem;
+	private int selectedRow;
 
 	public ObjectHistoryBean() {
 	}
 
+	public String takeSelection() {
+		selectedItems = new ArrayList<HistoryItem>();
+		Iterator<Object> iterator = getSelection().getKeys();
+		while (iterator.hasNext()) {
+			Object key = iterator.next();
+			table.setRowKey(key);
+			if (table.isRowAvailable()) {
+				getSelectedItems().add((HistoryItem) table.getRowData());
+			}
+		}
+		selectedItem = selectedItems.get(0);
+		return null;
+	}
+
 	public String showWorkspace() {
 		historyItems = null;
+		selectedItem = null;
 		return "show_workspace";
 	}
 
@@ -39,13 +59,25 @@ public class ObjectHistoryBean extends aDBbean {
 		if (historyItems == null || historyItems.size() == 0) {
 			historyItems = new ArrayList<HistoryItem>();
 			dbean.executeQuery(this);
-			// VObjectVersion selectedVersion = session.getSelectedVersion();
-			// List<VersionArc> precedors = selectedVersion.getPrecetorsArc();
-			// for (VersionArc arc : precedors) {
-			// historyItems.add(new HistoryItem(arc));
-			// }
 		}
 		return historyItems;
+	}
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public String executeQuery() {
+		VObject obj = session.getSelectedVersion().getVobject();
+		Query q = em.createNamedQuery("vovGetAllVersions");
+		q.setParameter("obj", obj);
+		List<VObjectVersion> res = (List<VObjectVersion>) q.getResultList();
+		for (VObjectVersion vov : res) {
+			List<VersionArc> precedors = vov.getPrecetorsArc();
+			for (VersionArc arc : precedors) {
+				historyItems.add(new HistoryItem(arc));
+			}
+		}
+		// TODO implement
+		return null;
 	}
 
 	public UIScrollableDataTable getTable() {
@@ -56,29 +88,36 @@ public class ObjectHistoryBean extends aDBbean {
 		this.table = table;
 	}
 
-	public HistoryItem getSelection() {
+	public SimpleSelection getSelection() {
 		return selection;
 	}
 
-	public void setSelection(HistoryItem selection) {
+	public void setSelection(SimpleSelection selection) {
 		this.selection = selection;
 	}
 
-	@SuppressWarnings("unchecked")
-	@Override
-	public String executeQuery() {
-		 VObject obj = session.getSelectedVersion().getVobject();
-		Query q = em.createNamedQuery("vovGetAllVersions");
-		q.setParameter("obj", obj);
-		List<VObjectVersion> res = (List<VObjectVersion>) q.getResultList();
-		for (VObjectVersion vov: res) {
-			List<VersionArc> precedors = vov.getPrecetorsArc();
-			for (VersionArc arc : precedors) {
-				 historyItems.add(new HistoryItem(arc));
-			}
-		}
-		//TODO implement
-		return null;
+	public int getSelectedRow() {
+		return selectedRow;
+	}
+
+	public void setSelectedRow(int selectedRow) {
+		this.selectedRow = selectedRow;
+	}
+
+	public HistoryItem getSelectedItem() {
+		return selectedItem;
+	}
+
+	public void setSelectedItem(HistoryItem selectedItem) {
+		this.selectedItem = selectedItem;
+	}
+
+	public ArrayList<HistoryItem> getSelectedItems() {
+		return selectedItems;
+	}
+
+	public void setSelectedItems(ArrayList<HistoryItem> selectedItems) {
+		this.selectedItems = selectedItems;
 	}
 
 }

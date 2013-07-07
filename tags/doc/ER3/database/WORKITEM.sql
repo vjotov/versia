11daf72bf4d818f7d91a8a525bec9ea3f077b4be
vjotov@gmail.com <vjotov@gmail.com@1393505e-1ba9-11df-8ce3-277986988311>
2013-07-07 03:30:14 +0000

    cleaning 1

--- tags/doc/ER3/database/WORKITEM.sql
+++
@@ -1,2 +0,0 @@
-REM INSERTING into WORKITEM
-Insert into WORKITEM (WI_ID,VO_ID) values (1,1);
--- tags/doc/ER3/database/WS_WI_SELECTOR.sql
+++
@@ -1,2 +0,0 @@
-REM INSERTING into WS_WI_SELECTOR
-Insert into WS_WI_SELECTOR (WS_ID,WI_ID) values (1,1);
